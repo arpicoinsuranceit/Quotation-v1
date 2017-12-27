602d742f18bf79714b1be4b04b84f51cc00ce20d
DELL <DELL@INS-IT89>
2017-12-27 11:06:31 +0530

    Add Session

--- Quotation/src/main/java/org/arpicoinsurance/groupit/main/listeners/SessionListener.java
+++ Quotation/src/main/java/org/arpicoinsurance/groupit/main/listeners/SessionListener.java
@@ -1,9 +1,11 @@
 package org.arpicoinsurance.groupit.main.listeners;
 
+import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 
+@WebListener
 public class SessionListener implements HttpSessionListener{
 
 	@Override
