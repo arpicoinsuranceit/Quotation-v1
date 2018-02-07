1f104406d844e2f88bb74f75b85eb22d96c36c08
Harindi <supunsara.harindi@gmail.com>
2018-02-07 09:36:53 +0530

    View Quotation

--- Quotation/src/main/java/org/arpicoinsurance/groupit/main/dao/QuotationDao.java
+++ Quotation/src/main/java/org/arpicoinsurance/groupit/main/dao/QuotationDao.java
@@ -1,10 +1,15 @@
 package org.arpicoinsurance.groupit.main.dao;
 
+import java.util.List;
+
+import org.apache.catalina.User;
 import org.arpicoinsurance.groupit.main.model.Quotation;
 import org.springframework.data.repository.CrudRepository;
 
 public interface QuotationDao extends CrudRepository<Quotation, Integer>{
 	
 	Quotation findById(Integer id) throws Exception;
+	
+	List<Quotation> findByUserWhereStatusOrderByIdDesc(User user,String status)throws Exception;
 
 }
