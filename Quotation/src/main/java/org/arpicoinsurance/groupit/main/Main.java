8d118fade8bca352c888000d14a5dfb11afe138f
DSP CHATHURANGA <dspchathuranga@gmail.com>
2018-02-08 14:17:17 +0530

    Tomcat deploy war file create

--- Quotation/src/main/java/org/arpicoinsurance/groupit/main/Main.java
+++ Quotation/src/main/java/org/arpicoinsurance/groupit/main/Main.java
@@ -2,12 +2,20 @@ package org.arpicoinsurance.groupit.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main{
+public class Main extends SpringBootServletInitializer{
 	
 	public static void main(String args []) {
 		SpringApplication.run(Main.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(Main.class);
 	}
 
 }
