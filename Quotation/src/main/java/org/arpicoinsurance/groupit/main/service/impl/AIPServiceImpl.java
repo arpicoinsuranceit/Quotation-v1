1189799a9f24b97bcb66bb444fb4f6bef85c9d90
DSPChathuranga <dspchathuranga@gmail.com>
2018-04-12 07:54:59 +0530

    aa

--- Quotation/src/main/java/org/arpicoinsurance/groupit/main/service/impl/AIPServiceImpl.java
+++ Quotation/src/main/java/org/arpicoinsurance/groupit/main/service/impl/AIPServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import org.arpicoinsurance.groupit.main.common.CalculationUtils;
 import org.arpicoinsurance.groupit.main.common.DateConverter;
-import org.arpicoinsurance.groupit.main.common.WebClient;
 import org.arpicoinsurance.groupit.main.dao.BenefitsDao;
 import org.arpicoinsurance.groupit.main.dao.CustomerDao;
 import org.arpicoinsurance.groupit.main.dao.CustomerDetailsDao;
@@ -32,7 +31,6 @@ import org.arpicoinsurance.groupit.main.service.AIPService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @Transactional
@@ -97,22 +95,27 @@ public class AIPServiceImpl implements AIPService {
 			System.out.println(" term : " + term + " adbrat : " + adbrat + " fundmarat : " + fundmarat + " intrat : "
 					+ intrat + " paymod : " + paymod);
 			aipCalShedules = new ArrayList<>();
-			for (int i = 1; i <= term.intValue(); i++) {
-				int polyear;
-				if (i > 3) {
-					polyear = 4;
-				} else {
-					polyear = i;
+
+			RateCardAIP rateCardAIP = null;
+
+			for (int i = 0; i < (term.intValue() * 12); i++) {
+				int polyear = 0;
+				if (i % 12 == 0) {
+					polyear = (polyear+1);
+					if (polyear > 3) {
+						polyear = 4;
+					}
 				}
 
-				RateCardAIP rateCardAIP = null;
+				System.out.println(" term : " + term + " polyear : " + polyear + " i % 12 " + i % 12 + " i % 3 " + i % 3
+						+ " i % 6 " + i % 6 + " i % 12 " + i % 12 + " (i / 12) " + (i / 12));
 
-				if (paymod.equalsIgnoreCase("S")) {
+				if (paymod.equalsIgnoreCase("S") && i == 0) {
 					rateCardAIP = rateCardAIPDao
 							.findByTermtoOrTermtoLessThanAndTermfromOrTermfromGreaterThanAndPaymodAndStrdatLessThanOrStrdat(
 									term.intValue(), term.intValue(), term.intValue(), term.intValue(), paymod, chedat,
 									chedat);
-				} else {
+				} else if (i % 12 == 0) {
 					rateCardAIP = rateCardAIPDao
 							.findByTermtoOrTermtoLessThanAndTermfromOrTermfromGreaterThanAndPaymodAndPolyearAndStrdatLessThanOrStrdat(
 									term.intValue(), term.intValue(), term.intValue(), term.intValue(), paymod, polyear,
@@ -122,90 +125,99 @@ public class AIPServiceImpl implements AIPService {
 				System.out.println(" term : " + term + " polyear : " + polyear + " Rate : " + rateCardAIP.getRate());
 				BigDecimal fund_rate = new BigDecimal(rateCardAIP.getRate().doubleValue());
 
-				for (int j = 1; j <= 12; j++) {
-					AipCalShedule aipCalShedule = new AipCalShedule();
-					if (schedule) {
-						System.out.println("polyer : " + i + " polmth : " + j + " opnfun : " + open_fund.toString());
-						aipCalShedule.setPolicyYear(i);
-						aipCalShedule.setPolicyMonth(j);
-						aipCalShedule.setOpeningFee(open_fund.setScale(0, BigDecimal.ROUND_UP).doubleValue());
-					}
+				// calculationUtils.getPayterm(paymod)
 
-					if ((paymod.equalsIgnoreCase("S")) && (i ==1)) {
-						fund_amount = new BigDecimal("0");
-						cum_premium = new BigDecimal("0");
-					} else if (paymod.equalsIgnoreCase("M")){
+				// for (int j = 1; j <= 12; j++) {
+				AipCalShedule aipCalShedule = new AipCalShedule();
+				if (schedule) {
+					System.out.println("polyer : " + ((i / 12) + 1) + " polmth : " + ((i % 12) + 1) + " opnfun : "
+							+ open_fund.toString());
+					aipCalShedule.setPolicyYear(((i / 12) + 1));
+					aipCalShedule.setPolicyMonth(((i % 12) + 1));
+					aipCalShedule.setOpeningFee(open_fund.setScale(0, BigDecimal.ROUND_UP).doubleValue());
+				}
+
+				if ((paymod.equalsIgnoreCase("S"))) {
+					if (i == 0) {
 						fund_amount = premium.multiply(fund_rate.divide(new BigDecimal("100"))).setScale(6, 4);
 						cum_premium = cum_premium.add(premium);
-					} else if (paymod.equalsIgnoreCase("Q")) {
-						if(i==1) {
-							fund_amount = premium.multiply(fund_rate.divide(new BigDecimal("100"))).setScale(6, 4);
-							cum_premium = cum_premium.add(premium);
-						} else if(i % 3 == 0) {
-							fund_amount = premium.multiply(fund_rate.divide(new BigDecimal("100"))).setScale(6, 4);
-							cum_premium = cum_premium.add(premium);
-						}
-					} else if (paymod.equalsIgnoreCase("H")) {
-						if(i==1) {
-							fund_amount = premium.multiply(fund_rate.divide(new BigDecimal("100"))).setScale(6, 4);
-							cum_premium = cum_premium.add(premium);
-						} else if(i % 6 == 0) {
-							fund_amount = premium.multiply(fund_rate.divide(new BigDecimal("100"))).setScale(6, 4);
-							cum_premium = cum_premium.add(premium);
-						}
-					} else if (paymod.equalsIgnoreCase("Y")) {
-						if(i==1) {
-							fund_amount = premium.multiply(fund_rate.divide(new BigDecimal("100"))).setScale(6, 4);
-							cum_premium = cum_premium.add(premium);
-						} 
+					} else {
+						fund_amount = new BigDecimal("0");
+						cum_premium = new BigDecimal("0");
 					}
-
-					balance_bfi = fund_amount.add(open_fund);
-
-					double interest = Math.pow(1.0D + interest_rate.divide(new BigDecimal("100")).doubleValue(),
-							1 / 12.0D) - 1.0D;
-
-					interest_annum = balance_bfi.multiply(new BigDecimal(interest)).setScale(6, 4);
-
-					balance_bmf = balance_bfi.add(interest_annum).setScale(6, 4);
-
-					mgt_fees = balance_bmf.multiply(management_fee.divide(new BigDecimal("100"))).setScale(6, 4);
-					close_bal = balance_bmf.subtract(mgt_fees).setScale(6, 4);
-
-					open_fund = new BigDecimal(close_bal.toPlainString()).setScale(6, 4);
-
-					if (schedule) {
-
-						System.out.println("cumcon : " + cum_premium.toString() + " fndamt : " + fund_amount.toString()
-								+ " fndbfi : " + balance_bfi.toString() + " intanm : " + interest_annum.toString()
-								+ " fndbmf : " + balance_bmf.toString() + " mgtfee : " + mgt_fees.toString()
-								+ " fndclo : " + close_bal.toString() + " fndclo : " + close_bal.toString());
-
-						aipCalShedule.setComCon(cum_premium.setScale(2, BigDecimal.ROUND_HALF_UP	).doubleValue());
-						aipCalShedule.setFundAmt(fund_amount.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
-						aipCalShedule.setFndBfi(balance_bfi.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
-						aipCalShedule.setFndBmf(balance_bmf.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
-						aipCalShedule.setMgtFee(mgt_fees.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
-						aipCalShedule.setFndClo(close_bal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
-						aipCalShedule.setIntAmt(interest_annum.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
-
-						if (close_bal.compareTo(cum_premium) == -1) {
-							System.out.println(
-									"adbcov : " + cum_premium.multiply(adb_rate).setScale(2, 4).toPlainString());
-
-							aipCalShedule.setAdbCov(cum_premium.multiply(adb_rate).setScale(2, 4).doubleValue());
-						} else {
-							System.out
-									.println("adbcov : " + close_bal.multiply(adb_rate).setScale(2, 4).toPlainString());
-
-							aipCalShedule.setAdbCov(close_bal.multiply(adb_rate).setScale(2, 4).doubleValue());
-						}
-						aipCalShedules.add(aipCalShedule);
+				} else if (paymod.equalsIgnoreCase("M")) {
+					fund_amount = premium.multiply(fund_rate.divide(new BigDecimal("100"))).setScale(6, 4);
+					cum_premium = cum_premium.add(premium);
+				} else if (paymod.equalsIgnoreCase("Q")) {
+					if (i % 3 == 0) {
+						fund_amount = premium.multiply(fund_rate.divide(new BigDecimal("100"))).setScale(6, 4);
+						cum_premium = cum_premium.add(premium);
+					} else {
+						fund_amount = new BigDecimal("0");
+						cum_premium = new BigDecimal("0");
 					}
-
-					total_amount = new BigDecimal(close_bal.toPlainString()).setScale(2, 4);
-
+				} else if (paymod.equalsIgnoreCase("H")) {
+					if (i % 6 == 0) {
+						fund_amount = premium.multiply(fund_rate.divide(new BigDecimal("100"))).setScale(6, 4);
+						cum_premium = cum_premium.add(premium);
+					} else {
+						fund_amount = new BigDecimal("0");
+						cum_premium = new BigDecimal("0");
+					}
+				} else if (paymod.equalsIgnoreCase("Y")) {
+					if (i % 12 == 0) {
+						fund_amount = premium.multiply(fund_rate.divide(new BigDecimal("100"))).setScale(6, 4);
+						cum_premium = cum_premium.add(premium);
+					} else {
+						fund_amount = new BigDecimal("0");
+						cum_premium = new BigDecimal("0");
+					}
 				}
+
+				balance_bfi = fund_amount.add(open_fund);
+
+				double interest = Math.pow(1.0D + interest_rate.divide(new BigDecimal("100")).doubleValue(), 1 / 12.0D)
+						- 1.0D;
+
+				interest_annum = balance_bfi.multiply(new BigDecimal(interest)).setScale(6, 4);
+
+				balance_bmf = balance_bfi.add(interest_annum).setScale(6, 4);
+
+				mgt_fees = balance_bmf.multiply(management_fee.divide(new BigDecimal("100"))).setScale(6, 4);
+				close_bal = balance_bmf.subtract(mgt_fees).setScale(6, 4);
+
+				open_fund = new BigDecimal(close_bal.toPlainString()).setScale(6, 4);
+
+				if (schedule) {
+
+					System.out.println("cumcon : " + cum_premium.toString() + " fndamt : " + fund_amount.toString()
+							+ " fndbfi : " + balance_bfi.toString() + " intanm : " + interest_annum.toString()
+							+ " fndbmf : " + balance_bmf.toString() + " mgtfee : " + mgt_fees.toString() + " fndclo : "
+							+ close_bal.toString() + " fndclo : " + close_bal.toString());
+
+					aipCalShedule.setComCon(cum_premium.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
+					aipCalShedule.setFundAmt(fund_amount.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
+					aipCalShedule.setFndBfi(balance_bfi.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
+					aipCalShedule.setFndBmf(balance_bmf.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
+					aipCalShedule.setMgtFee(mgt_fees.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
+					aipCalShedule.setFndClo(close_bal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
+					aipCalShedule.setIntAmt(interest_annum.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue());
+
+					if (close_bal.compareTo(cum_premium) == -1) {
+						System.out.println("adbcov : " + cum_premium.multiply(adb_rate).setScale(2, 4).toPlainString());
+
+						aipCalShedule.setAdbCov(cum_premium.multiply(adb_rate).setScale(2, 4).doubleValue());
+					} else {
+						System.out.println("adbcov : " + close_bal.multiply(adb_rate).setScale(2, 4).toPlainString());
+
+						aipCalShedule.setAdbCov(close_bal.multiply(adb_rate).setScale(2, 4).doubleValue());
+					}
+					aipCalShedules.add(aipCalShedule);
+				}
+
+				total_amount = new BigDecimal(close_bal.toPlainString()).setScale(2, 4);
+
+				// }
 
 			}
 
@@ -221,7 +233,9 @@ public class AIPServiceImpl implements AIPService {
 			aipCalResp.setAipCalShedules(aipCalShedules);
 			aipCalResp.setExtraOe(adminFee + tax);
 			return aipCalResp;
-		} finally {
+		} finally
+
+		{
 			if (aipCalResp != null) {
 				aipCalResp = null;
 			}
@@ -264,7 +278,6 @@ public class AIPServiceImpl implements AIPService {
 			customer.setCustModifyBy(user.getUserCode());
 			customer.setCustModifyDate(new Date());
 			customer.setCustName(_invpSaveQuotation.get_mainlife().get_mName());
-			customer.setCustCode(new WebClient().getCustCode(_invpSaveQuotation));
 
 			customerDetails = getCustomerDetail(occupation, _invpSaveQuotation, user);
 			customerDetails.setCustomer(customer);
