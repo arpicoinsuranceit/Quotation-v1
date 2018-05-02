e92f0f8ea394c28289cab8b118f3efd382de0f13
Harindi Supunsara <supunsara.harindi@gmail.com>
2018-05-02 13:53:35 +0530

    Fix Bug at HRBF

--- Quotation/src/main/java/org/arpicoinsurance/groupit/main/service/custom/impl/QuotationSaveUtilServiceImpl.java
+++ Quotation/src/main/java/org/arpicoinsurance/groupit/main/service/custom/impl/QuotationSaveUtilServiceImpl.java
@@ -475,7 +475,8 @@ public class QuotationSaveUtilServiceImpl implements QuotationSaveUtilService {
 						break;
 					case "HCBFS":
 						benef_Details.setRiderPremium(calResp.getHrbfs());
-						if(benef_Details.getRiderSum().equals(null)) {
+						System.out.println(benef_Details.getRiderSum());
+						if(benef_Details.getRiderSum()==null) {
 							benef_Details.setRiderSum(0.0);
 						}
 						benef_Details.setRiderTerm(calResp.getHrbfsTerm());
@@ -590,7 +591,7 @@ public class QuotationSaveUtilServiceImpl implements QuotationSaveUtilService {
 					 */
 					case "HCBFC":
 						benef_Details.setRiderPremium(0.0);
-						if(benef_Details.getRiderSum().equals(null)) {
+						if(benef_Details.getRiderSum()==null) {
 							benef_Details.setRiderSum(0.0);
 						}
 						benef_Details.setRierCode(type);
