50e23863553ea23168aca3802815db4e4c7a339f
Anjana Thrishakya <anjanathrishakya@gmail.com>
2018-06-07 11:36:37 +0530

    ARTM EDIT Initial

--- Quotation/src/main/java/org/arpicoinsurance/groupit/main/controller/QuotationArtmCalculationController.java
+++ Quotation/src/main/java/org/arpicoinsurance/groupit/main/controller/QuotationArtmCalculationController.java
@@ -147,6 +147,67 @@ public class QuotationArtmCalculationController {
 	}
 	
 	
+	@RequestMapping(value = "/quoArtmEdit/{userId}/{qdId}", method = RequestMethod.POST)
+	public ResponseEntity<Object> editARTM(@RequestBody InvpSaveQuotation _invpSaveQuotation,
+			@PathVariable("userId") Integer userId, @PathVariable("qdId") Integer qdId) {
+
+		HashMap<String, Object> responseMap = new HashMap<>();
+		responseMap.put("status", "fail");
+		QuotationCalculation calculation = null;
+
+		Validation validation = null;
+		try {
+			if (userId != null) {
+				if (_invpSaveQuotation.get_calPersonalInfo() != null) {
+					calculation = new QuotationCalculation();
+					calculation.set_personalInfo(_invpSaveQuotation.get_calPersonalInfo());
+					calculation.set_riderDetails(_invpSaveQuotation.get_riderDetails());
+					calculation.set_product(_invpSaveQuotation.get_product());
+					validation = new Validation(calculation);
+					String valError = validation.validateArtm(calculation);
+					if(valError.equals("ok")) {
+						String error = validation.validateBenifict();
+						if (error.equals("No")) {
+							responseMap = artmService.editQuotation(calculation, _invpSaveQuotation, userId, qdId);
+						} else {
+							responseMap.replace("status", error);
+						}
+					} else {
+						responseMap.replace("status", valError);
+					}
+				} else {
+					responseMap.replace("status", "Incomplete");
+				}
+			} else {
+				responseMap.replace("status", "User can't be identify");
+
+			}
+			return new ResponseEntity<Object> (responseMap, HttpStatus.CREATED);
+		} catch (Exception e) {
+			Logs logs = new Logs();
+			logs.setData("Error : " + e.getMessage() + ",\n Parameters : _invpSaveQuotation : " + _invpSaveQuotation.toString() + " userId : " + userId + " qdId : " + qdId);
+			logs.setDate(new Date());
+			logs.setHeading("Error");
+			logs.setOperation("editEnd : QuotationEndCalculationController");
+			try {
+				logService.saveLog(logs);
+			} catch (Exception e1) {
+				System.out.println("... Error Message for Operation ...");
+				e.printStackTrace();
+				System.out.println("... Error Message for save log ...");
+				e1.printStackTrace();
+			}
+			return new ResponseEntity<Object> (e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		} finally {
+			if (calculation != null) {
+				calculation = null;
+			}
+			if (validation != null) {
+				validation = null;
+			}
+		}
+	}
+	
 	
 	/*@RequestMapping(value = "/artmshedule", method = RequestMethod.POST)
 	public ArrayList<AipCalShedule> loadSheduleATRM (@RequestBody Plan plan) {
@@ -163,53 +224,6 @@ public class QuotationArtmCalculationController {
 		return null;
 	}
 	
-	@RequestMapping(value = "/artmSavequo/{id}", method = RequestMethod.POST)
-	public HashMap<String, Object> saveAIP(@RequestBody InvpSavePersonalInfo _invpSaveQuotation, @PathVariable Integer id) {
-		HashMap<String, Object> responseMap = new HashMap<>();
-		responseMap.put("status", "fail");
-		try {
-			if (id != null) {
-				if (_invpSaveQuotation != null) {
-					Validation validation = new Validation();
-					if(validation.validateArtm(_invpSaveQuotation.get_plan()).equals("ok")) {
-						return artmService.saveQuotation(_invpSaveQuotation, id);
-					}
-				}
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-
-		}
-		return responseMap;
-	}
-	
-	@RequestMapping(value = "/quoArtmEdit/{userId}/{qdId}", method = RequestMethod.POST)
-	public HashMap<String, Object> editAip(@RequestBody InvpSavePersonalInfo _invpSaveQuotation, @PathVariable("userId") Integer userId,
-			@PathVariable("qdId") Integer qdId) {
-		HashMap<String, Object> responseMap = new HashMap<>();
-		responseMap.put("status", "fail");
-		try {
-			if (userId != null) {
-				if (qdId != null) {
-					if (_invpSaveQuotation != null) {
-						Validation validation = new Validation();
-						if(validation.validateArtm(_invpSaveQuotation.get_plan()).equals("ok")) {
-							return artmService.editQuotation(_invpSaveQuotation, userId, qdId);
-						}
-					}
-				}
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-
-		}
-		return responseMap;
-	}*/
-	
-
+	*/
 	
 }
