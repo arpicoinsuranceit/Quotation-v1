0576033e3602ddad51325f29cc38088718b8f120
Anjana Thrishakya <anjanathrishakya@gmail.com>
2018-06-07 10:24:17 +0530

    ARTM SAVE

--- Quotation/src/main/java/org/arpicoinsurance/groupit/main/controller/QuotationArtmCalculationController.java
+++ Quotation/src/main/java/org/arpicoinsurance/groupit/main/controller/QuotationArtmCalculationController.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import org.arpicoinsurance.groupit.main.helper.AIPCalResp;
 import org.arpicoinsurance.groupit.main.helper.AipCalShedule;
 import org.arpicoinsurance.groupit.main.helper.InvpSavePersonalInfo;
+import org.arpicoinsurance.groupit.main.helper.InvpSaveQuotation;
 import org.arpicoinsurance.groupit.main.helper.Plan;
 import org.arpicoinsurance.groupit.main.helper.QuotationCalculation;
 import org.arpicoinsurance.groupit.main.helper.QuotationQuickCalResponse;
@@ -85,7 +86,65 @@ public class QuotationArtmCalculationController {
 		
 	}
 	
-	
+	@RequestMapping(value = "/artmSavequo/{id}", method = RequestMethod.POST)
+	public ResponseEntity<Object> saveARTM(@RequestBody InvpSaveQuotation _invpSaveQuotation,
+			@PathVariable Integer id) {
+		HashMap<String, Object> responseMap = new HashMap<>();
+		responseMap.put("status", "fail");
+
+		QuotationCalculation calculation = null;
+		Validation validation = null;
+		try {
+			if (id != null) {
+				if (_invpSaveQuotation.get_calPersonalInfo() != null) {
+					calculation = new QuotationCalculation();
+					calculation.set_personalInfo(_invpSaveQuotation.get_calPersonalInfo());
+					calculation.set_riderDetails(_invpSaveQuotation.get_riderDetails());
+					calculation.set_product(_invpSaveQuotation.get_product());
+					validation = new Validation(calculation);
+					String valError = validation.validateArtm(calculation);
+					if(valError.equals("ok")) {
+						String error = validation.validateBenifict();
+						if (error.equals("No")) {
+							responseMap = artmService.saveQuotation(calculation, _invpSaveQuotation, id);
+						} else {
+							responseMap.replace("status", error);
+						}
+					} else {
+						responseMap.replace("status", valError);
+					}
+				} else {
+					responseMap.replace("status", "Incomplete");
+				}
+			} else {
+				responseMap.replace("status", "User can't be identify");
+
+			}
+			return new ResponseEntity<Object> (responseMap, HttpStatus.CREATED);
+		} catch (Exception e) {
+			Logs logs = new Logs();
+			logs.setData("Error : " + e.getMessage() + ",\n Parameters : _invpSaveQuotation : " + _invpSaveQuotation.toString() + " id : " + id);
+			logs.setDate(new Date());
+			logs.setHeading("Error");
+			logs.setOperation("saveEnd : QuotationEndCalculationController");
+			try {
+				logService.saveLog(logs);
+			} catch (Exception e1) {
+				System.out.println("... Error Message for Operation ...");
+				e.printStackTrace();
+				System.out.println("... Error Message for save log ...");
+				e1.printStackTrace();
+			}
+			return new ResponseEntity<Object> (e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		} finally {
+			if (calculation != null) {
+				calculation = null;
+			}
+			if (validation != null) {
+				validation = null;
+			}
+		}
+	}
 	
 	
 	
