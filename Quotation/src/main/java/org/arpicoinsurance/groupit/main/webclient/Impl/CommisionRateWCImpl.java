ce8218ada3ffe8175fcd01a4a01df9f819716a7e
DSPChathuranga <dspchathuranga@gmail.com>
2018-07-02 11:03:16 +0530

    a

--- Quotation/src/main/java/org/arpicoinsurance/groupit/main/webclient/Impl/CommisionRateWCImpl.java
+++ Quotation/src/main/java/org/arpicoinsurance/groupit/main/webclient/Impl/CommisionRateWCImpl.java
@@ -14,7 +14,7 @@ public class CommisionRateWCImpl implements CommisionRateWC {
 	public HashMap<String, Double> getCommisionRate(CommisionRatePara commisionRatePara) throws Exception {
 		try {
 			System.out.println(commisionRatePara.getPrdcod()+" "+commisionRatePara.getComyer()+" "+commisionRatePara.getToterm());
-			final String uri = "http://localhost:8085/commisionRate";
+			final String uri = "http://10.10.10.12:8080/Infosys/commisionRate";
 			//final String uri = "http://localhost:8085/commisionRate";
 			RestTemplate restTemplate = new RestTemplate();
 			HashMap<String, Double> result = restTemplate.postForObject(uri, commisionRatePara, HashMap.class);
