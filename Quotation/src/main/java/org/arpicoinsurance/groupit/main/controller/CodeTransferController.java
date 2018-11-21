8068f3930a147a0bc350fe0514013b45f66b517b
Anjana Thrishakya <anjanathrishakya@gmail.com>
2018-11-21 16:47:38 +0530

    Change Controller

--- Quotation/src/main/java/org/arpicoinsurance/groupit/main/controller/CodeTransferController.java
+++ Quotation/src/main/java/org/arpicoinsurance/groupit/main/controller/CodeTransferController.java
@@ -3,10 +3,13 @@ package org.arpicoinsurance.groupit.main.controller;
 import java.util.List;
 
 import org.arpicoinsurance.groupit.main.helper.CodeTransferHelper;
+import org.arpicoinsurance.groupit.main.helper.CodeTransferReqHelper;
 import org.arpicoinsurance.groupit.main.webclient.ReceiptClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,23 +22,21 @@ public class CodeTransferController {
 	@Autowired
 	private ReceiptClient receiptClient;
 	
-	@RequestMapping(value="/code_transfer/getCodeTransfersToApprove", method = RequestMethod.POST)
-	public List<CodeTransferHelper> getCodeTransfersToApprove(@RequestParam("userCode")String userCode)throws Exception{
+	@RequestMapping(value="/code_transfer/getCodeTransfersToApprove/{userCode:.+}", method = RequestMethod.GET)
+	public List<CodeTransferHelper> getCodeTransfersToApprove(@PathVariable String userCode)throws Exception{
 		return receiptClient.getCodeTransferToApprove(userCode);
 	}
     
 	@RequestMapping(value="/code_transfer/rejectCodeTransfer", method = RequestMethod.POST)
-	public ResponseEntity<Object> rejectCodeTran(@RequestParam("user")String user,@RequestParam("codeTransferId")String codeTransferId
-			,@RequestParam("remark")String remark)throws Exception{
+	public ResponseEntity<Object> rejectCodeTran(@RequestBody CodeTransferReqHelper reqHelper)throws Exception{
 		
-		return receiptClient.rejectCodeTransfer(user,codeTransferId,remark);
+		return receiptClient.rejectCodeTransfer(reqHelper.getUser(),reqHelper.getTransId(),reqHelper.getRemark());
 	}
 	
 	@RequestMapping(value="/code_transfer/approveCodeTransfer", method = RequestMethod.POST)
-	public ResponseEntity<Object> approveCodeTran(@RequestParam("user")String user,@RequestParam("codeTransferId")String codeTransferId
-			,@RequestParam("remark")String remark)throws Exception{
+	public ResponseEntity<Object> approveCodeTran(@RequestBody CodeTransferReqHelper reqHelper)throws Exception{
 		
-		return receiptClient.approveCodeTransfer(user,codeTransferId,remark);
+		return receiptClient.approveCodeTransfer(reqHelper.getUser(),reqHelper.getTransId(),reqHelper.getRemark());
 	}
 	
 }
