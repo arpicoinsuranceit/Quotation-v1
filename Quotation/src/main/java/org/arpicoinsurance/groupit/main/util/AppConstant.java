6739ec0b21fcaf27f7682b031322aaeaf51db20d
DSP Chathuranga <dspchathuranga@gmail.com>
2018-12-03 08:50:55 +0530

    V 3.1.1

--- Quotation/src/main/java/org/arpicoinsurance/groupit/main/util/AppConstant.java
+++ Quotation/src/main/java/org/arpicoinsurance/groupit/main/util/AppConstant.java
@@ -3,14 +3,14 @@ package org.arpicoinsurance.groupit.main.util;
 public class AppConstant {
 	
 
-	public final static String URL_GET_CUST_CODE = "http://10.10.10.12:8085/testABC";
-	public final static String URL_LAST_YEAR_RATE = "http://10.10.10.12:8085/lstYearRate";
-	public final static String URL_COMMISION_RATE = "http://10.10.10.12:8085/commisionRate";
-	public final static String URL_BENEFICT_HISTORY = "http://10.10.10.12:8085/getbeneficthistory";
-	public final static String URL_GET_BRANCH = "http://10.10.10.12:8089/getBranches";
-	public final static String URL_GET_CODE_TRANSFERS = "http://10.10.10.12:8089/code_transfer/getCodeTransfersToApprove";
-	public final static String URL_GET_CODE_TRANSFER_APPROVE = "http://10.10.10.12:8089/code_transfer/approveCodeTran";
-	public final static String URL_GET_CODE_TRANSFER_REJECT = "http://10.10.10.12:8089/code_transfer/rejectCodeTran";
+	public final static String URL_GET_CUST_CODE = "http://10.10.10.11:8087/testABC";
+	public final static String URL_LAST_YEAR_RATE = "http://10.10.10.11:8087/lstYearRate";
+	public final static String URL_COMMISION_RATE = "http://10.10.10.11:8087/commisionRate";
+	public final static String URL_BENEFICT_HISTORY = "http://10.10.10.11:8087/getbeneficthistory";
+	public final static String URL_GET_BRANCH = "http://10.10.10.11:8089/getBranches";
+	public final static String URL_GET_CODE_TRANSFERS = "http://10.10.10.11:8089/code_transfer/getCodeTransfersToApprove";
+	public final static String URL_GET_CODE_TRANSFER_APPROVE = "http://10.10.10.11:8089/code_transfer/approveCodeTran";
+	public final static String URL_GET_CODE_TRANSFER_REJECT = "http://10.10.10.11:8089/code_transfer/rejectCodeTran";
 
 
 }
